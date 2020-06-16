6c02e7faf565bce0dc74a30031952ecb365e42b8
Diego Tiller <diegotiller@live.com>
2020-06-16 19:30:11 -0300

    Upload de fotos com ajax

--- uploadFotosAjax/script.js
+++ uploadFotosAjax/script.js
@@ -3,10 +3,12 @@ $(function(){
 	$('button').on('click', function(){
 		var data = new FormData();
 
+		//var arquivos = $('input[name=foto]')[0].files; ou
 		var arquivos = $('#foto')[0].files;
 
 		if(arquivos.length > 0) {
 
+			//data.append('nome', $('input[name=nome]').val());
 			data.append('nome', $('#nome').val());
 
 			data.append('foto', arquivos[0]);
